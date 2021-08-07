cb9f6f5da12b2471341589a3e737f9bee8628aed
Aleksandr <mahnev.web@gmail.com>
2021-08-07 22:52:08 +0700

    Burger_upd2.5

--- app/js/burger.js
+++ app/js/burger.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$('.nav-bttn-burger').click(function(){
-		$('.nav-bttn-bar').toggleClass('open-menu');
-		$('.nav-bttn-bar').toggleClass('fixed-page');
+		$('.nav-bttn-bar').toggleClass('open-menu fixed-page');
+		$(this).toggleClass('open-menu');
 	});
 });
